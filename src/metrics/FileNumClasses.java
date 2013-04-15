6f5f36a8cd2cfb431587a897a93dcadc5e41efb7
Jorge Delgado <jorgdelga@hotmail.com>
2013-04-15 16:02:54 +0100

    Create Util

--- src/metrics/FileNumClasses.java
+++ src/metrics/FileNumClasses.java
@@ -8,10 +8,11 @@ import java.io.IOException;
 public class FileNumClasses {
 
     private String[] listWords;
+    private Util util = new Util();
 
     public int getFileClass(String file) throws IOException {
         int num = 0;
-        BufferedReader br = getBufferTextLines(file);
+        BufferedReader br = util.getBufferTextLines(file);
         String linea;
         while ((linea = br.readLine()) != null) {
             if (linea.contains("class")) {
@@ -23,7 +24,7 @@ public class FileNumClasses {
 
     public int getFileImports(String file) throws IOException {
         int num = 0;
-        BufferedReader br = getBufferTextLines(file);
+        BufferedReader br = util.getBufferTextLines(file);
         String linea;
         while ((linea = br.readLine()) != null) {
             if (linea.contains("import")) {
@@ -33,13 +34,8 @@ public class FileNumClasses {
         return num;
     }
 
-    public String[] getWords(String line) {
-        String[] words = line.split(" ");//[pal1,pal2,pal3]
-        return words;
-    }
-
     public boolean isFunction(String line) {
-        listWords = getWords(line);
+        listWords = util.getWords(line);
         if (listWords.length != 0) {
             if (line.contains("public") || line.contains("private") || line.contains("protected") && line.contains("{")) {
                 return true;
@@ -49,19 +45,9 @@ public class FileNumClasses {
         return false;
     }
 
-    public BufferedReader getBufferTextLines(String file) throws IOException {
-        File archivo = null;
-        FileReader fr = null;
-        BufferedReader br = null;
-        archivo = new File(file);
-        fr = new FileReader(archivo);
-        br = new BufferedReader(fr);
-        return br;
-    }
-
     public int getFunctions(String file) throws IOException {
         int num = 0;
-        BufferedReader br = getBufferTextLines(file);
+        BufferedReader br = util.getBufferTextLines(file);
         String linea;
         while ((linea = br.readLine()) != null) {
             if (isFunction(linea)) {
