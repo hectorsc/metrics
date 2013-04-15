2b96945469202720405df647542178b4e5d44e9a
Jorge <Jorge@Jorge-PC>
2013-04-15 18:56:40 +0100

    Construct

---
+++ src/metrics/FileConstructsParams.java
@@ -0,0 +1,64 @@
+package metrics;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+
+public class FileConstructsParams {
+    
+    private String[] listWords;
+    private Util util = new Util();
+    
+    public boolean isConstruct(String line, String path) {
+        line.getClass();
+        if (line.contains(path.concat("("))) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int getConstrucs(String file) throws IOException {
+        int num = 0;
+        BufferedReader br = util.getBufferTextLines(file);
+        String linea;
+        File f = new File(file);
+        String[] namefile = f.getName().split(".java");
+        String path = namefile[0];
+        while ((linea = br.readLine()) != null) {
+            if (isConstruct(linea, path)) {
+                num++;
+            }
+        }   
+        return num;
+    }
+    
+    public boolean isParam(String line, String path) {
+        line.getClass();
+        listWords = util.getWords(line);
+        for(String pal: listWords)
+            System.out.println(pal);
+        if (listWords.length != 0) {
+            if (line.contains(path.concat("("))) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+    
+    public int getParams(String file) throws IOException {
+        int num = 0;
+        BufferedReader br = util.getBufferTextLines(file);
+        String linea;
+        File f = new File(file);
+        String[] namefile = f.getName().split(".java");
+        String path = namefile[0];
+        while ((linea = br.readLine()) != null) {
+            if (isParam(linea, path)) {
+                num++;
+            }
+        }   
+        return num;
+    }
+    
+}
