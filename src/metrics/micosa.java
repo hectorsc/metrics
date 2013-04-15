3cbcf85ef36038a76012882e2c40bc27bc2822f5
Hector <Hector@10.22.146.224>
2013-04-15 17:54:12 +0100

    micosa

--- src/metrics/micosa.java
+++ src/metrics/micosa.java
@@ -4,11 +4,29 @@
  */
 package metrics;
 
-/**
- *
- * @author Eugenio
- */
+import java.io.IOException;
+
 public class micosa {
-    
-    
+
+    private Util util = new Util();
+
+    public int getFors(String file) throws IOException {
+        return util.getFilePattern(file, "for");
+    }
+
+    public int getIfs(String file) throws IOException {
+        return util.getFilePattern(file, "if");
+    }
+
+    public int getWhiles(String file) throws IOException {
+        return util.getFilePattern(file, "while");
+    }
+
+    public int getGlobals(String file) throws IOException {
+        return util.getFilePattern(file, "global");
+    }
+
+    public int getSwitchs(String file) throws IOException {
+        return util.getFilePattern(file, "switch");
+    }
 }
