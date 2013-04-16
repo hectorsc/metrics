9bd922935e0d64551703be8b0fc35c02e76881aa
yeray <yeray@10.37.129.2>
2013-04-16 13:26:44 +0100

    fixed 2

--- src/metrics/Util.java
+++ src/metrics/Util.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package metrics;
 
 import java.io.BufferedReader;
@@ -9,10 +5,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-/**
- *
- * @author Jorge
- */
 public class Util {
 
     public Util() {
